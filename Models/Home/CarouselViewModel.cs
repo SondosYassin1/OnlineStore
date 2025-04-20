1243379e22233ef29066ca06f9d9879f30fbdf3b
HP <HP@DESKTOP-DL24RK5>
2025-04-20 11:36:20 +0300

    Special offers

---
+++ Models/Home/CarouselViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineStore.Models.Home
+{
+    public class CarouselItemViewModel
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string ImageUrl { get; set; }
+        public string LinkUrl { get; set; }
+        public string ButtonText { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    public class CarouselViewModel
+    {
+        public IEnumerable<CarouselItemViewModel> Items { get; set; }
+    }
+
+}
---
+++ Models/Home/SpecialOfferViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineStore.Models.Home
+{
+    public class SpecialOfferViewModel
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public string ButtonText { get; set; }
+        public string Category { get; set; }
+        public string BackgroundColor { get; set; } = "bg-danger";
+    }
+}
