ce763bef5a64b0123a4e9cb27a7e56c5c132b0ff
HP <HP@DESKTOP-DL24RK5>
2025-04-20 16:55:09 +0300

    Complete the views of home page

---
+++ Models/Home/TestimonialViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineStore.Models.Home
+{
+    public class TestimonialViewModel
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Comment { get; set; }
+        public decimal Rating { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
